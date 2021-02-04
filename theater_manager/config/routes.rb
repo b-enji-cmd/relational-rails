10a0ea5e46e9e821efb4e61fa358d55dfe533272
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-04 15:15:20 -0700

    Add PATCH route to edit theaters

--- theater_manager/config/routes.rb
+++ theater_manager/config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   get	'/theaters', to: 'theaters#index'
   get '/theaters/new', to: 'theaters#new'
   get '/auditoriums', to: 'auditoriums#index'
+  get '/theaters/:id', to: 'theaters#show'
+  get '/theaters/:id/edit', to: 'theaters#edit'
 
   post '/theaters', to: 'theaters#create'
+
+  patch '/theaters/:id', to: 'theaters#update'
 end
 
 # have a localized welcome page, that has two routes to Theaters and Audits
