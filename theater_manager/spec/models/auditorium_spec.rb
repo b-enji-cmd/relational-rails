94075380f540b01f150326843fa600659022d613
Gvieve <gnuebel84@yahoo.com>
2021-02-07 01:26:49 -0700

    Add realtionship model test for auditorium

--- theater_manager/spec/models/auditorium_spec.rb
+++ theater_manager/spec/models/auditorium_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Auditorium, type: :model do
-  # describe 'relationships' do
-  #   it { should have_many :movies }
-  # end
+  describe 'relationships' do
+    it { should have_many :movies }
+  end
 end
