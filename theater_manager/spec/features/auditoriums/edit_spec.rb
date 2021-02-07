fa5c7406a2e4bd2e048aca876f4f9738407fa1fb
Gvieve <gnuebel84@yahoo.com>
2021-02-06 23:19:11 -0700

    Add sad path testing for aduitoriums#edit

--- theater_manager/spec/features/auditoriums/edit_spec.rb
+++ theater_manager/spec/features/auditoriums/edit_spec.rb
@@ -45,4 +45,67 @@ RSpec.describe 'When I visit auditorium index', type: :feature do
     expect(page).to have_content("Capacity: 101")
     expect(page).to have_content("IMAX Auditorium: Yes")
   end
+
+  it 'I can update an auditorium but name is not changed if empty' do
+    Auditorium.destroy_all
+
+    auditorium_1 = Auditorium.create( name: "North 1",
+                                      capacity: 50,
+                                      is_imax_auditorium: true)
+    visit "/auditoriums"
+
+    click_link("North 1")
+    click_link("Edit Auditorium")
+    fill_in('auditorium[name]', with: '')
+    fill_in('auditorium[capacity]', with: '')
+    choose('auditorium_is_imax_auditorium_false')
+    click_button('Update Auditorium')
+
+    expect(current_path).to eq("/auditoriums/#{auditorium_1.id}")
+    expect(page).to have_content("North 1")
+    expect(page).to have_content("Capacity: 50")
+    expect(page).to have_content("IMAX Auditorium: No")
+
+    click_link("Edit Auditorium")
+    fill_in('auditorium[name]', with: '')
+    fill_in('auditorium[capacity]', with: '75')
+    choose('auditorium_is_imax_auditorium_false')
+    click_button('Update Auditorium')
+
+    expect(current_path).to eq("/auditoriums/#{auditorium_1.id}")
+    expect(page).to have_content("North 1")
+    expect(page).to have_content("Capacity: 75")
+    expect(page).to have_content("IMAX Auditorium: No")
+  end
+
+  it 'I can update an auditorium but capacity is not changed if empty' do
+    Auditorium.destroy_all
+
+    auditorium_1 = Auditorium.create( name: "North 1",
+                                      capacity: 50,
+                                      is_imax_auditorium: true)
+    visit "/auditoriums"
+
+    click_link("North 1")
+    click_link("Edit Auditorium")
+    fill_in('auditorium[capacity]', with: '')
+    choose('auditorium_is_imax_auditorium_false')
+    click_button('Update Auditorium')
+
+    expect(current_path).to eq("/auditoriums/#{auditorium_1.id}")
+    expect(page).to have_content("North 1")
+    expect(page).to have_content("Capacity: 50")
+    expect(page).to have_content("IMAX Auditorium: No")
+
+    click_link("Edit Auditorium")
+    fill_in('auditorium[name]', with: 'North 2')
+    fill_in('auditorium[capacity]', with: '')
+    choose('auditorium_is_imax_auditorium_true')
+    click_button('Update Auditorium')
+
+    expect(current_path).to eq("/auditoriums/#{auditorium_1.id}")
+    expect(page).to have_content("North 2")
+    expect(page).to have_content("Capacity: 50")
+    expect(page).to have_content("IMAX Auditorium: Yes")
+  end
 end
