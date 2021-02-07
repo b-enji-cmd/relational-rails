326da5c4d8e048a61e658b68e6acfa10fc0f8d35
Gvieve <gnuebel84@yahoo.com>
2021-02-07 13:59:30 -0700

    Add edit and update methods in movies_controller.rb

--- theater_manager/app/controllers/movies_controller.rb
+++ theater_manager/app/controllers/movies_controller.rb
@@ -6,4 +6,20 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    movie = Movie.find(params[:id])
+
+    params[:movie].each do |key, value|
+      if !value.empty? && key != "auditorium_id"
+        movie.update!({:"#{key}" => value})
+      end
+    end
+
+    redirect_to "/movies/#{movie.id}"
+  end
 end
