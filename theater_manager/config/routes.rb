f0c68f301fe0009a0b5fee8c1ed89efc1ba3a24a
Ben Fulton <benjamin.fulton99@gmail.com>
2021-02-07 10:16:09 -0700

    Update schema to reflect parent child relationship

--- theater_manager/config/routes.rb
+++ theater_manager/config/routes.rb
@@ -2,20 +2,31 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
-  get	'/theaters', to: 'theaters#index'
-  get '/theaters/new', to: 'theaters#new'
   get '/auditoriums', to: 'auditoriums#index'
   get '/auditoriums/new', to: 'auditoriums#new'
   get '/auditoriums/:id', to: 'auditoriums#show'
+ 
+  get '/theaters', to: 'theaters#index'
   get '/theaters/:id', to: 'theaters#show'
+  get '/theaters/new', to: 'theaters#new'
   get '/theaters/:id/edit', to: 'theaters#edit'
+
+  get '/employees', to: 'employees#index'
+  get '/employees/:id', to: 'employees#show'
+  get '/theaters/:id/employees', to: 'employees#index'
+  get '/theaters/:id/employees/new', to: 'employees#new'
+  get '/employees/:id/edit', to: 'employee#edit'
+  
 
   post '/theaters', to: 'theaters#create'
   post '/auditoriums', to: 'auditoriums#create'
+  post '/employees', to: 'employees#create'
 
   patch '/theaters/:id', to: 'theaters#update'
+  patch '/employees/:id', to: 'employees#update'
 
   delete '/theaters/:id', to: 'theaters#destroy'
+  delete '/employees/:id', to: 'employees#destroy'
 
 end
 
--- theater_manager/db/schema.rb
+++ theater_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_235658) do
+ActiveRecord::Schema.define(version: 2021_02_05_185138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2021_02_03_235658) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_full_time"
+    t.integer "hours_worked"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "theater_id"
+    t.index ["theater_id"], name: "index_employees_on_theater_id"
+  end
+
   create_table "theaters", force: :cascade do |t|
     t.string "name"
     t.boolean "is_open"
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_02_03_235658) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "employees", "theaters"
 end
