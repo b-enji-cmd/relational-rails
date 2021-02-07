0d61294b32045472b150aebf1c152444d7215498
Gvieve <gnuebel84@yahoo.com>
2021-02-06 19:41:08 -0700

    Create feature tests for Auditorium new and index

--- theater_manager/spec/features/auditoriums/index_spec.rb
+++ theater_manager/spec/features/auditoriums/index_spec.rb
@@ -17,19 +17,35 @@ RSpec.describe 'auditoriums index page', type: :feature do
     expect(page).to have_content(auditorium_2.name)
   end
 
-  it 'has link to auditoriums/new' do
+  it 'has link and redirects to auditoriums/new' do
     visit 'auditoriums'
 
     expect(page).to have_link(href: '/auditoriums/new')
+
+    click_link 'New Auditorium'
+    save_and_open_page
+
+    expect(page).to have_current_path('/auditoriums/new')
   end
 
-  it 'has link to auditoriums/:id' do
+  it 'has link to auditoriums/:id and displays all Auditorium data' do
     auditorium_1 = Auditorium.create( name: "North 1",
                                       capacity: 50,
                                       is_imax_auditorium: true)
     visit 'auditoriums'
+
+    expect(page).to have_link(href: "/auditoriums/#{auditorium_1.id}")
+
+    click_link 'North 1'
     save_and_open_page
 
+    expect(page).to have_link(href: '/auditoriums')
+    expect(page).to have_content("Auditorium Index")
+    expect(page).to have_content("North 1")
+    expect(page).to have_content("Capacity: 50")
+    expect(page).to have_content("IMAX Auditorium: Yes")
+    expect(page).to have_content("Edit Auditorium")
+    expect(page).to have_content("Delete Auditorium")
     expect(page).to have_link(href: "/auditoriums/#{auditorium_1.id}")
   end
 end
---
+++ theater_manager/spec/features/auditoriums/new_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'auditoriums new page', type: :feature do
+
+  it 'can display form for name, capacity, IMAX auditorium fields' do
+    visit '/auditoriums/new'
+
+    expect(page).to have_content("Enter a new auditorium:")
+    expect(page).to have_content("Name: ")
+    expect(page).to have_content("Capacity: ")
+    expect(page).to have_content("IMAX Auditorium: ")
+    expect(page).to have_content("Required field")
+  end
+
+  it 'creates a new auditorium' do
+    visit '/auditoriums/new'
+
+    fill_in('auditorium[name]', with: 'South 1')
+    fill_in('auditorium[capacity]', with: '100')
+    choose('true')
+    click_button('Create Auditorium')
+    save_and_open_page
+
+    expect(page).to have_content('South 1')
+  end
+
+  it 'does not create new auditorium when name or capacity are empty' do
+    visit '/auditoriums/new'
+    # save_and_open_page
+
+    choose('true')
+    click_button('Create Auditorium')
+
+    expect(page).to have_content("Auditorium not created: Required information missing.")
+    expect(page).to have_button('Create Auditorium')
+
+    fill_in('auditorium[capacity]', with: '100')
+    choose('true')
+    click_button('Create Auditorium')
+
+    expect(page).to have_content("Auditorium not created: Required information missing.")
+    expect(page).to have_button('Create Auditorium')
+
+    fill_in('auditorium[name]', with: 'South 2')
+    choose('true')
+    click_button('Create Auditorium')
+
+    expect(page).to have_content("Auditorium not created: Required information missing.")
+    expect(page).to have_button('Create Auditorium')
+  end
+end
