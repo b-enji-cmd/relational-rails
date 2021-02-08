0d89a439e26c2a64ced7bd3d2c83c91ba9bb78c5
Gvieve <gnuebel84@yahoo.com>
2021-02-07 22:06:00 -0700

    Add permit movie_params method, include in update

--- theater_manager/app/controllers/movies_controller.rb
+++ theater_manager/app/controllers/movies_controller.rb
@@ -14,8 +14,8 @@ class MoviesController < ApplicationController
   def update
     movie = Movie.find(params[:id])
 
-    params[:movie].each do |key, value|
-      if !value.empty? && key != "auditorium_id"
+    movie_params.each do |key, value|
+      if !value.empty?
         movie.update!({:"#{key}" => value})
       end
     end
@@ -26,5 +26,16 @@ class MoviesController < ApplicationController
   def destroy
     Movie.destroy(params[:id])
     redirect_to "/movies"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name,
+                                  :showtime_date,
+                                  :showtime_start,
+                                  :duration,
+                                  :ticket_cost,
+                                  :is_rated_r)
   end
 end
