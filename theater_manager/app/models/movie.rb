4e6178000f45688e052cd222eca35a70464e26c4
Gvieve <gnuebel84@yahoo.com>
2021-02-07 22:35:28 -0700

    Add validations for additional columns in Movie

--- theater_manager/app/models/movie.rb
+++ theater_manager/app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  validates :name, :showtime_start, :duration, presence: true
+  validates :name, :showtime_date, :showtime_start, :duration, :ticket_cost, presence: true
 
   belongs_to :auditorium
 end
